d3f47153c9d86c747f327497effc7e781f70de2f
wuguanjun <1967908224@qq.com>
2015-10-19 15:41:13 +0800

    动态修改布局

--- app/src/main/java/com/magus/trainingfirstapp/activity/TrainingFirstActivity.java
+++ app/src/main/java/com/magus/trainingfirstapp/activity/TrainingFirstActivity.java
@@ -14,8 +14,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
@@ -33,6 +35,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import static com.magus.trainingfirstapp.R.string.intent_test;
+
 public class TrainingFirstActivity extends BaseActivity {
 
 
@@ -44,6 +48,28 @@ public class TrainingFirstActivity extends BaseActivity {
     private static final int REQUEST_IMAGE_CAPTURE_O = 2;
     private ImageView takePhotoThenToShowImg;
 
+    private LinearLayout llt_funbtn;
+    private int[] ids = new int[]{R.id.fragment_btn, R.id.intent_text_btn,R.id.intent_map_btn,
+            R.id.intent_web_btn,
+            R.id.intent_email,
+            R.id.first_open_new_activity_btn,
+            R.id.first_share_btn,
+            R.id.first_take_phote_demo,
+            R.id.first_take_phote_btn,
+            R.id.first_take_photo_o_btn,
+            R.id.open_swipe_menu_btn
+    };
+       private String[] btnStrs = new String[]{"fragment","intentTest","map","webView","email","OpenNewActivityBtn","ShareButton",
+//            getResources().getString(R.string.intent_test),
+//            getResources().getString(R.string.map).toString(),
+//            getResources().getString(R.string.web).toString(),
+//            getResources().getString(R.string.email).toString(),
+//            getResources().getString(R.string.opennew).toString(),
+//            getResources().getString(R.string.sharebutton).toString(),
+            "TakePhoto_压缩","TakePhotoDemo","TakePhoto_原图","OpenSwipeMenuDemo"
+//            getResources().getString(R.string.open_swipe_demo).toString()
+    };
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,13 +77,25 @@ public class TrainingFirstActivity extends BaseActivity {
         final int first_photo_show_img = R.id.first_photo_show_img;
         takePhotoThenToShowImg = (ImageView) findViewById(first_photo_show_img);
 
-        getBitmapBounds();
+        llt_funbtn = (LinearLayout) findViewById(R.id.llt_funbtn);
 
+        for(int i=0;i<ids.length;i++){
+            addButton(ids[i],btnStrs[i]);
+        }
+        getBitmapBounds();
 
     }
 
+    private void addButton(int id, String btnStr) {
+        Button button = new Button(this);
+        button.setText(btnStr);
+        button.setId(id);
+        button.setOnClickListener(this);
+        llt_funbtn.addView(button);
+    }
+
     /**
-     *   为了避免java.lang.OutOfMemory 的异常，在真正解析图片之前检查它的尺寸
+     * 为了避免java.lang.OutOfMemory 的异常，在真正解析图片之前检查它的尺寸
      */
     private void getBitmapBounds() {
 
@@ -71,10 +109,8 @@ public class TrainingFirstActivity extends BaseActivity {
     }
 
 
-
-
-    public void sendMessage(View view){
-        Intent intent = new Intent(TrainingFirstActivity.this,OtherActivity.class);
+    public void sendMessage(View view) {
+        Intent intent = new Intent(TrainingFirstActivity.this, OtherActivity.class);
         EditText editText = (EditText) findViewById(R.id.message_et);
         String message = editText.getText().toString();
         intent.putExtra(G.MessageConst.MESSAGE, message);
@@ -95,7 +131,7 @@ public class TrainingFirstActivity extends BaseActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 showToast("action_settings");
                 break;
@@ -112,7 +148,7 @@ public class TrainingFirstActivity extends BaseActivity {
     @Override
     public void onClick(View v) {
         super.onClick(v);
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.activity_first_btn:
                 showToast("大家好");
                 startActivity(new Intent(TrainingFirstActivity.this, ActivityA.class));
@@ -122,14 +158,14 @@ public class TrainingFirstActivity extends BaseActivity {
                 break;
             case R.id.intent_text_btn:
                 Uri number = Uri.parse("tel:110");
-                startActivity(new Intent(Intent.ACTION_DIAL,number));
+                startActivity(new Intent(Intent.ACTION_DIAL, number));
                 break;
             case R.id.intent_map_btn:
                 try {
                     Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
                     startActivity(new Intent(Intent.ACTION_VIEW, location));
 
-                }catch (Exception e){
+                } catch (Exception e) {
                     showToast("没有地图");
                     return;
                 }
@@ -147,7 +183,7 @@ public class TrainingFirstActivity extends BaseActivity {
                 startActivity(otherintent);
                 break;
             case R.id.first_share_btn:
-                startActivity(Intent.createChooser( shareText(), "选啊你"));
+                startActivity(Intent.createChooser(shareText(), "选啊你"));
                 break;
             case R.id.open_swipe_menu_btn:
                 startActivity(new Intent(TrainingFirstActivity.this, SwipeMenuDemoActvity.class));
@@ -165,14 +201,14 @@ public class TrainingFirstActivity extends BaseActivity {
                 break;
             case R.id.first_take_photo_o_btn:   //保存拍摄到的原图
                 Intent takePictureOintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (takePictureOintent.resolveActivity(getPackageManager()) != null){
+                if (takePictureOintent.resolveActivity(getPackageManager()) != null) {
                     File photoFile = null;
                     try {
                         photoFile = createImageFile();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                    if (photoFile != null){
+                    if (photoFile != null) {
                         takePictureOintent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
                         startActivityForResult(takePictureOintent, REQUEST_IMAGE_CAPTURE_O);
                     }
@@ -196,7 +232,7 @@ public class TrainingFirstActivity extends BaseActivity {
         return shareIntent;
     }
 
-    public void sendMsg(String content){
+    public void sendMsg(String content) {
         SmsManager smsManager = SmsManager.getDefault();
         List<String> divideContents = smsManager.divideMessage(content);
         for (String text : divideContents) {
@@ -206,7 +242,7 @@ public class TrainingFirstActivity extends BaseActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE_O && resultCode == RESULT_OK){
+        if (requestCode == REQUEST_IMAGE_CAPTURE_O && resultCode == RESULT_OK) {
             int targetW = takePhotoThenToShowImg.getWidth();
             int targetH = takePhotoThenToShowImg.getHeight();
 
@@ -220,7 +256,7 @@ public class TrainingFirstActivity extends BaseActivity {
 		/* Figure out which way needs to be reduced less */
             int scaleFactor = 1;
             if ((targetW > 0) || (targetH > 0)) {
-                scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+                scaleFactor = Math.min(photoW / targetW, photoH / targetH);
             }
 
 		/* Set bitmap options to scale the image decode target */
@@ -235,7 +271,7 @@ public class TrainingFirstActivity extends BaseActivity {
             takePhotoThenToShowImg.setImageBitmap(bitmap);
             galleryAddPic();
         }
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){//展示图片
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {//展示图片
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             takePhotoThenToShowImg.setImageBitmap(imageBitmap);
@@ -246,10 +282,11 @@ public class TrainingFirstActivity extends BaseActivity {
 
     /**
      * 创建拍摄的图片的存储路径及文件名
+     *
      * @return
      * @throws IOException
      */
-    private File createImageFile() throws IOException{
+    private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
@@ -263,7 +300,7 @@ public class TrainingFirstActivity extends BaseActivity {
     /**
      * 将拍摄到的照片添加到Media Provider的数据库中
      */
-    private void galleryAddPic(){
+    private void galleryAddPic() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(mCurrentPhotoPath);
         Uri contentUri = Uri.fromFile(f);
