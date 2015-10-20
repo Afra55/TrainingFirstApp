18b01587ed0aff666d7c4839e7910e3cad4df989
Afra55 <1937525930@qq.com>
2015-10-20 13:11:07 +0800

    修改架构

--- app/src/main/java/com/magus/trainingfirstapp/base/TrainingFirstActivity.java
+++ app/src/main/java/com/magus/trainingfirstapp/base/TrainingFirstActivity.java
@@ -40,99 +40,36 @@ import java.util.List;
 
 public class TrainingFirstActivity extends BaseActivity {
 
-
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    /**
-     * 拍照flag
-     */
-    private static final int REQUEST_IMAGE_CAPTURE_O = 2;
     private ImageView takePhotoThenToShowImg;
-
     private LinearLayout llt_funbtn;
-
-    private int[] ids = new int[]{
-            R.id.fragment_btn,
-            R.id.intent_text_btn,
-            R.id.intent_map_btn,
-            R.id.intent_web_btn,
-            R.id.intent_email,
-            R.id.first_open_new_activity_btn,
-            R.id.first_share_btn,
-            R.id.first_take_phote_demo,
-            R.id.first_take_phote_btn,
-            R.id.first_take_photo_o_btn,
-            R.id.open_swipe_menu_btn,
-            R.id.circlemenu
-    };
-    private String[] btnStrs = new String[]{
-            "fragment",
-            "intentTest",
-            "map",
-            "webView",
-            "email",
-            "OpenNewActivityBtn",
-            "ShareButton",
-//            getResources().getString(R.string.intent_test),
-//            getResources().getString(R.string.map).toString(),
-//            getResources().getString(R.string.web).toString(),
-//            getResources().getString(R.string.email).toString(),
-//            getResources().getString(R.string.opennew).toString(),
-//            getResources().getString(R.string.sharebutton).toString(),
-            "TakePhoto_压缩",
-            "TakePhotoDemo",
-            "TakePhoto_原图",
-            "OpenSwipeMenuDemo",
-            "仿建行原形菜单"
-//            getResources().getString(R.string.open_swipe_demo).toString()
-    };
+    private final int MODULE_BUTTON_ITEM_ID = G.FlagsConst.BUTTON_ITEM_ID;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.activity_training_first);
-        final int first_photo_show_img = R.id.first_photo_show_img;
-        takePhotoThenToShowImg = (ImageView) findViewById(first_photo_show_img);
+        llt_funbtn = (LinearLayout) findViewById(R.id.first_module_content_lly);
+        takePhotoThenToShowImg = (ImageView) findViewById(R.id.first_image_content);
 
-        llt_funbtn = (LinearLayout) findViewById(R.id.llt_funbtn);
-
-        for(int i=0;i<ids.length;i++){
-            addButton(ids[i],btnStrs[i]);
+        // 添加所有的按钮模块
+        for(int i=0; i<G.ModuleConst.FIRST_ACTIVITY_MODULE_BUTTON_COUNT; i++){
+            addButton(i);
         }
-        getBitmapBounds();
-
     }
 
-    private void addButton(int id, String btnStr) {
+    /**
+     * 添加模块按钮
+     * @param tagId
+     */
+    private void addButton(int tagId) {
         Button button = new Button(this);
-        button.setText(btnStr);
-        button.setId(id);
+        button.setText(G.getModuleBtnName(this, tagId));
+        button.setId(MODULE_BUTTON_ITEM_ID);
+        button.setTag(tagId);
         button.setOnClickListener(this);
         llt_funbtn.addView(button);
     }
 
-    /**
-     * 为了避免java.lang.OutOfMemory 的异常，在真正解析图片之前检查它的尺寸
-     */
-    private void getBitmapBounds() {
-
-        BitmapFactory.Options options = new BitmapFactory.Options();
-
-        // 这个设置会返回一个null的Bitmap，但是可以获取到 outWidth, outHeight 与 outMimeType
-        options.inJustDecodeBounds = true;
-        BitmapFactory.decodeResource(getResources(), R.drawable.zhizhuxia, options);
-        String textContent = "imageHeigth = " + options.outHeight + " \nimageWidth = " + options.outWidth + " \nmimeType = " + options.outMimeType;
-        ((TextView) findViewById(R.id.first_bitmap_decode_options_tv)).setText(textContent);
-    }
-
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(TrainingFirstActivity.this, OtherActivity.class);
-        EditText editText = (EditText) findViewById(R.id.message_et);
-        String message = editText.getText().toString();
-        intent.putExtra(G.MessageConst.MESSAGE, message);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -165,58 +102,58 @@ public class TrainingFirstActivity extends BaseActivity {
     @Override
     public void onClick(View v) {
         super.onClick(v);
-        switch (v.getId()) {
-            case R.id.activity_first_btn:
-                showToast("大家好");
-                startActivity(new Intent(TrainingFirstActivity.this, ActivityA.class));
+        Intent intent = null;
+        switch (v.getId()){
+            case MODULE_BUTTON_ITEM_ID:
+                intent = onModuleBtnClick((Integer) v.getTag());
                 break;
-            case R.id.fragment_btn:
-                startActivity(new Intent(this, FragmentMainActivity.class));
-                break;
-            case R.id.intent_text_btn:
+        }
+        if (intent!=null){
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * 模块的点击事件
+     * @param tagId
+     */
+    private Intent onModuleBtnClick(int tagId) {
+        switch (tagId) {
+            case 0:
+                return new Intent(this, FragmentMainActivity.class);
+            case 1:
                 Uri number = Uri.parse("tel:110");
-                startActivity(new Intent(Intent.ACTION_DIAL, number));
-                break;
-            case R.id.intent_map_btn:
+                return new Intent(Intent.ACTION_DIAL, number);
+            case 2:
                 try {
                     Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
-                    startActivity(new Intent(Intent.ACTION_VIEW, location));
-
+                    return new Intent(Intent.ACTION_VIEW, location);
                 } catch (Exception e) {
                     showToast("没有地图");
-                    return;
+                    return null;
                 }
-                break;
-            case R.id.intent_web_btn:
+            case 3:
                 Uri webPage = Uri.parse("http://www.baidu.com");
-                startActivity(new Intent(Intent.ACTION_VIEW, webPage));
-                break;
-            case R.id.intent_email:
+                return  new Intent(Intent.ACTION_VIEW, webPage);
+            case 4:
                 sendMsg("ssss");
-                break;
-            case R.id.first_open_new_activity_btn:
+            case 5:
                 Intent otherintent = new Intent(TrainingFirstActivity.this, OtherActivity.class);
                 otherintent.putExtra("key", "key i come on");
-                startActivity(otherintent);
-                break;
-            case R.id.first_share_btn:
-                startActivity(Intent.createChooser(shareText(), "选啊你"));
-                break;
-            case R.id.open_swipe_menu_btn:
-                startActivity(new Intent(TrainingFirstActivity.this, SwipeMenuDemoActvity.class));
-                break;
-
-            case R.id.first_take_phote_demo:
-                startActivity(new Intent(TrainingFirstActivity.this, PhotoIntentActivity.class));
-                break;
-
-            case R.id.first_take_phote_btn:
+                return otherintent;
+            case 6:
+                return Intent.createChooser(shareText(), "选啊你");
+            case 10:
+                return new Intent(TrainingFirstActivity.this, SwipeMenuDemoActvity.class);
+            case 7:
+                return new Intent(TrainingFirstActivity.this, PhotoIntentActivity.class);
+            case 8:
                 Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                    startActivityForResult(takePictureIntent, G.FlagsConst.REQUEST_IMAGE_CAPTURE);
                 }
-                break;
-            case R.id.first_take_photo_o_btn:   //保存拍摄到的原图
+                return null;
+            case 9:   //保存拍摄到的原图
                 Intent takePictureOintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePictureOintent.resolveActivity(getPackageManager()) != null) {
                     File photoFile = null;
@@ -227,24 +164,32 @@ public class TrainingFirstActivity extends BaseActivity {
                     }
                     if (photoFile != null) {
                         takePictureOintent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                        startActivityForResult(takePictureOintent, REQUEST_IMAGE_CAPTURE_O);
+                        startActivityForResult(takePictureOintent, G.FlagsConst.REQUEST_IMAGE_CAPTURE_O);
                     }
                 }
-                break;
-            case R.id.first_open_bitmap_and_anim_activity:
-                startActivity(new Intent(TrainingFirstActivity.this, DisplayingBitmapsActivity.class));
-                break;
-            case R.id.first_my_anim_demo_btn:
-                startActivity(new Intent(TrainingFirstActivity.this, MyAnimActivity.class));
-                break;
-            case R.id.circlemenu:
-                startActivity(new Intent(TrainingFirstActivity.this, CircleMenuActivity.class));
-                break;
+                return null;
+            case 11:
+                return new Intent(TrainingFirstActivity.this, CircleMenuActivity.class);
+            case 12:
+                showToast("大家好");
+                return new Intent(TrainingFirstActivity.this, ActivityA.class);
+            case 13:
+                return new Intent(TrainingFirstActivity.this, DisplayingBitmapsActivity.class);
+            case 14:
+                return new Intent(TrainingFirstActivity.this, MyAnimActivity.class);
         }
+        return null;
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(TrainingFirstActivity.this, OtherActivity.class);
+        EditText editText = (EditText) findViewById(R.id.message_et);
+        String message = editText.getText().toString();
+        intent.putExtra(G.MessageConst.MESSAGE, message);
+        startActivity(intent);
     }
 
     private Intent shareText() {
-
         Intent shareIntent = new Intent();
         shareIntent.setAction(Intent.ACTION_SEND);
         shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.hellowShuaiYang));
@@ -262,7 +207,7 @@ public class TrainingFirstActivity extends BaseActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE_O && resultCode == RESULT_OK) {
+        if (requestCode == G.FlagsConst.REQUEST_IMAGE_CAPTURE_O && resultCode == RESULT_OK) {
             int targetW = takePhotoThenToShowImg.getWidth();
             int targetH = takePhotoThenToShowImg.getHeight();
 
@@ -291,7 +236,7 @@ public class TrainingFirstActivity extends BaseActivity {
             takePhotoThenToShowImg.setImageBitmap(bitmap);
             galleryAddPic();
         }
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {//展示图片
+        if (requestCode == G.FlagsConst.REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {//展示图片
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             takePhotoThenToShowImg.setImageBitmap(imageBitmap);
--- app/src/main/java/com/magus/trainingfirstapp/base/field/G.java
+++ app/src/main/java/com/magus/trainingfirstapp/base/field/G.java
@@ -1,19 +1,79 @@
 package com.magus.trainingfirstapp.base.field;
 
+import android.content.Context;
+
+import com.magus.trainingfirstapp.R;
+
 /**
  * Created by yangshuai on 2015/9/28 0028.
  */
 public class G {
     public class HostConst{
-        private static final boolean isQA = true;
+        public static final boolean isQA = true;
     }
     public interface MessageConst{
         public static final String MESSAGE = "message";
     }
     public interface FlagsConst{
-        public static final int SHOW_TOASH = 0;
-        public static final int INT_LINE_FLAG = 1;
-        public static final int INT_SUB_VIEW_FLAG = 2;
+        public static final int REQUEST_IMAGE_CAPTURE = 1;
+        /**
+         * 拍照flag
+         */
+        public static final int REQUEST_IMAGE_CAPTURE_O = 2;
+        public static final int BUTTON_ITEM_ID = 1001;
+    }
+
+    /**
+     * 模块常量
+     */
+    public interface ModuleConst{
+
+        /**
+         * 主界面模块个数(按钮点击相关)
+         */
+        public static final int FIRST_ACTIVITY_MODULE_BUTTON_COUNT = 15;
+    }
+
+    /**
+     * 通过tagId获取模块名
+     * @param context
+     * @param tagId
+     * @return
+     */
+    public static String getModuleBtnName(Context context, int tagId){
+        switch (tagId){
+            case 0:
+                return context.getResources().getString(R.string.fragment);
+            case 1:
+                return context.getResources().getString(R.string.intent_test);
+            case 2:
+                return context.getResources().getString(R.string.map);
+            case 3:
+                return context.getResources().getString(R.string.web);
+            case 4:
+                return context.getResources().getString(R.string.email);
+            case 5:
+                return context.getResources().getString(R.string.opennew);
+            case 6:
+                return context.getResources().getString(R.string.sharebutton);
+            case 7:
+                return context.getResources().getString(R.string.take_photo_s);
+            case 8:
+                return context.getResources().getString(R.string.take_photo_demo);
+            case 9:
+                return context.getResources().getString(R.string.take_photo_o);
+            case 10:
+                return context.getResources().getString(R.string.open_swipe_demo);
+            case 11:
+                return context.getResources().getString(R.string.imitate_ccb_menu);
+            case 12:
+                return context.getResources().getString(R.string.activity_life);
+            case 13:
+                return context.getResources().getString(R.string.img_anim_more);
+            case 14:
+                return context.getResources().getString(R.string.my_anim);
+        }
+        return "";
     }
 
 }
