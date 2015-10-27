da2e42ac7cedc18a0a6a1fe4fbee9ba1325072a7
Afra55 <1937525930@qq.com>
2015-10-27 10:08:01 +0800

    兼容性优化

--- app/src/main/java/com/magus/trainingfirstapp/module/animdemo/ScreenSlideActivity.java
+++ app/src/main/java/com/magus/trainingfirstapp/module/animdemo/ScreenSlideActivity.java
@@ -16,12 +16,12 @@
 
 package com.magus.trainingfirstapp.module.animdemo;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -65,7 +65,7 @@ public class ScreenSlideActivity extends FragmentActivity {
 
         // Instantiate a ViewPager and a PagerAdapter.
         mPager = (ViewPager) findViewById(R.id.pager);
-        mPagerAdapter = new ScreenSlidePagerAdapter(getFragmentManager());
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
         mPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
