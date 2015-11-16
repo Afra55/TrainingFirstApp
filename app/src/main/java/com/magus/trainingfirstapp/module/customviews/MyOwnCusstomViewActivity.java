e6ea790bae8e510d337146c851067a1d1733b353
杨帅 <yangshuai@funguide.com.cn>
2015-11-16 14:46:00 +0800

    自定义Scroller

--- app/src/main/java/com/magus/trainingfirstapp/module/customviews/MyOwnCusstomViewActivity.java
+++ app/src/main/java/com/magus/trainingfirstapp/module/customviews/MyOwnCusstomViewActivity.java
@@ -2,15 +2,26 @@ package com.magus.trainingfirstapp.module.customviews;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.magus.trainingfirstapp.R;
 import com.magus.trainingfirstapp.base.BaseActivity;
 
 public class MyOwnCusstomViewActivity extends BaseActivity {
 
+    private CusstomScrollView mScrollView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentLayout(R.layout.activity_my_own_cusstom_view);
+        mScrollView = (CusstomScrollView) findViewById(R.id.mCusstomScrollview);
+        for (int i = 0; i < 3; i++) {
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+            imageView.setImageResource(R.drawable.loading);
+            mScrollView.addView(imageView);
+        }
     }
 }
