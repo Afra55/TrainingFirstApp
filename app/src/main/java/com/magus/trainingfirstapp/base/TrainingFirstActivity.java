10c481c4dbfcf3ba3776b8db4771b58f8cf4597d
杨帅 <yangshuai@funguide.com.cn>
2015-12-03 10:34:41 +0800

    优化主界面

--- app/src/main/java/com/magus/trainingfirstapp/base/TrainingFirstActivity.java
+++ app/src/main/java/com/magus/trainingfirstapp/base/TrainingFirstActivity.java
@@ -22,13 +22,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.ShareActionProvider;
 import android.widget.TextView;
 
@@ -42,7 +42,6 @@ import com.magus.trainingfirstapp.module.customviews.CusstomViewActivity;
 import com.magus.trainingfirstapp.module.customviews.CusstomViewTestActivity;
 import com.magus.trainingfirstapp.module.customviews.ViewDragHelperTestActivity;
 import com.magus.trainingfirstapp.module.effectivenavigation.EffectiveNavigationActivity;
-import com.magus.trainingfirstapp.module.frgment_demo.FragmentMainActivity;
 import com.magus.trainingfirstapp.module.image_handle.ImageHandlePixActivity;
 import com.magus.trainingfirstapp.module.images.DisplayingBitmapsActivity;
 import com.magus.trainingfirstapp.module.myanim.MyAnimActivity;
@@ -53,9 +52,9 @@ import com.magus.trainingfirstapp.module.pagerSlidingTabStrip.PagerSlidingTabStr
 import com.magus.trainingfirstapp.module.photobyintent.PhotoIntentActivity;
 import com.magus.trainingfirstapp.module.pingme.PingMeActivity;
 import com.magus.trainingfirstapp.module.swipe_menu.SwipeMenuDemoActvity;
-import com.magus.trainingfirstapp.utils.CommontUtils;
 import com.magus.trainingfirstapp.utils.alert_utils.AlertUtils;
 import com.magus.trainingfirstapp.utils.download_utils.DownLoadService;
+import com.magus.trainingfirstapp.view.AutoDisplayChildViewContainer;
 import com.networkbench.agent.impl.NBSAppAgent;
 
 import java.io.File;
@@ -67,8 +66,9 @@ import java.util.List;
 public class TrainingFirstActivity extends BaseActivity {
 
     private ImageView takePhotoThenToShowImg;
-    private LinearLayout first_module_content_lly;
+    private AutoDisplayChildViewContainer first_module_content_lly;
     private final int MODULE_BUTTON_ITEM_ID = G.FlagsConst.BUTTON_ITEM_ID;
+    private ScrollView scrollView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -84,10 +84,15 @@ public class TrainingFirstActivity extends BaseActivity {
         setActionBarLeftBtnText("Exit");
         setActionBarTitle("主界面");
         setActionBarRightBtnText("look sha");
-        first_module_content_lly = (LinearLayout) findViewById(R.id.first_module_content_lly);
+        initWidget();
+        initModule();
+    }
+
+    private void initWidget() {
+        first_module_content_lly = (AutoDisplayChildViewContainer) findViewById(R.id.first_module_content_lly);
         takePhotoThenToShowImg = (ImageView) findViewById(R.id.first_image_content);
         ((TextView) findViewById(R.id.first_show_tv)).setText(Build.MODEL);
-        initModule();
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
     }
 
     private void initModule() {
@@ -415,6 +420,8 @@ public class TrainingFirstActivity extends BaseActivity {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             takePhotoThenToShowImg.setImageBitmap(imageBitmap);
+            showToast("图片已经显示在蓝色布景上");
+            scrollView.smoothScrollTo(0, 0);
         }
     }
 
