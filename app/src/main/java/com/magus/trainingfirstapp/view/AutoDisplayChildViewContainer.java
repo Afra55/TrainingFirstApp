668a7735d1891f4d9602174bcc5749c3970d13fc
杨帅 <yangshuai@funguide.com.cn>
2015-12-03 12:01:28 +0800

    AutoDisplayChildViewContainer

--- app/src/main/java/com/magus/trainingfirstapp/view/AutoDisplayChildViewContainer.java
+++ app/src/main/java/com/magus/trainingfirstapp/view/AutoDisplayChildViewContainer.java
@@ -1,7 +1,6 @@
 package com.magus.trainingfirstapp.view;
 
 import android.content.Context;
-import android.hardware.camera2.TotalCaptureResult;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +9,7 @@ import com.magus.trainingfirstapp.utils.DisplayUtil;
 
 /**
  * Created by yangshuai in the 17:25 of 2015.12.02 .
+ * 自动分配子view的位置, 具有自动换行,水平居中的功能
  */
 public class AutoDisplayChildViewContainer extends ViewGroup {
 
@@ -46,6 +46,7 @@ public class AutoDisplayChildViewContainer extends ViewGroup {
             View child = getChildAt(i);
             if (child.getVisibility() != View.GONE) {
 
+                /* 测量子布局 */
                 if (child.getMeasuredWidth() > parentWidth) {
                     child.measure(parentWidth, child.getMeasuredHeight());
                 } else
@@ -61,6 +62,7 @@ public class AutoDisplayChildViewContainer extends ViewGroup {
             }
         }
 
+        /* 获取适配子布局后的高度 */
         int parentHeight = tempTotalHeight + tempMaxChildHeight + margin;
         setMeasuredDimension(parentWidth, parentHeight);
     }
@@ -89,23 +91,26 @@ public class AutoDisplayChildViewContainer extends ViewGroup {
             View child = getChildAt(i);
             if (child.getVisibility() != View.GONE) {
 
-                maxChildHeight = Math.max(maxChildHeight, child.getMeasuredHeight());
                 if (i != 0) {
+                    /* child 的 left 是上个子 view 的宽加上 margin */
                     totaleft += getChildAt(i - 1).getMeasuredWidth() + margin;
                 } else {
                     totaleft = 0;
                     totalTop = 0;
+                    maxChildHeight = child.getMeasuredHeight();
                 }
+                /* child 的 right */
                 totalRight = totaleft + child.getMeasuredWidth();
-
+                /* 如果 rigth 大于 父布局的宽， 则换行 */
                 if (totalRight > parentWidth) {
-
-                    adjustLine(lineViewCount, i);
-                    lineViewCount = 0;
+                    adjustLine(lineViewCount, i); // 调整这一行的子布局的位置
+                    lineViewCount = 0;  // 这一行的子 child 的数量充值
                     totalTop += maxChildHeight;
                     totaleft = 0;
                     maxChildHeight = child.getMeasuredHeight();
                     totalRight = child.getMeasuredWidth();
+                }else {
+                    maxChildHeight = Math.max(maxChildHeight, child.getMeasuredHeight());
                 }
 
                 child.layout(
@@ -114,21 +119,31 @@ public class AutoDisplayChildViewContainer extends ViewGroup {
                         totalRight,
                         totalTop + child.getMeasuredHeight()
                 );
+
+                /* 统计这一行的子view的数量 */
                 lineViewCount++;
             }
         }
 
-        totaleft= totalRight + margin;
+        /* 调整最后一行子布局的位置 */
+        totaleft = totalRight + margin;
         adjustLine(lineViewCount, count);
 
     }
 
+    /* 调整一行，让这一行的子布局水平居中 */
     private void adjustLine(int lineViewCount, int i) {
         totaleft = (parentWidth - totaleft) / 2;
+        int marginTop;
         for (int lineViewNumber = lineViewCount; lineViewNumber > 0; lineViewNumber--) {
             View lineViewChild = getChildAt(i - lineViewNumber);
             totalRight = totaleft + lineViewChild.getMeasuredWidth();
-            lineViewChild.layout(totaleft, totalTop, totalRight, totalTop + lineViewChild.getMeasuredHeight());
+            if (lineViewChild.getMeasuredHeight() != maxChildHeight) {
+                marginTop = (maxChildHeight - lineViewChild.getMeasuredHeight()) / 2;
+            } else {
+                marginTop = 0;
+            }
+            lineViewChild.layout(totaleft, totalTop + marginTop, totalRight, totalTop + marginTop + lineViewChild.getMeasuredHeight());
             totaleft += lineViewChild.getMeasuredWidth() + margin;
         }
     }
