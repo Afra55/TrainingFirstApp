216ecf8015f3d237f31e6e8ca9b4833ba8e92c93
杨帅 <yangshuai@funguide.com.cn>
2016-01-11 16:19:42 +0800

    FactoryMode

---
+++ app/src/main/java/com/magus/trainingfirstapp/design_mode/factory_mode/BaseFacory.java
@@ -0,0 +1,9 @@
+package com.magus.trainingfirstapp.design_mode.factory_mode;
+
+/**
+ * Created by yangshuai in the 16:05 of 2016.01.11 .
+ * 抽象类
+ */
+public abstract class BaseFacory {
+    public abstract void start();
+}
