3367e0adb8937d0e5022287f586801a339b41744
杨帅 <yangshuai@funguide.com.cn>
2016-01-29 12:54:02 +0800

    策略模式

---
+++ app/src/main/java/com/magus/trainingfirstapp/design_mode/strategy_pattern_mode/Strategy.java
@@ -0,0 +1,8 @@
+package com.magus.trainingfirstapp.design_mode.strategy_pattern_mode;
+
+/**
+ * Created by yangshuai in the 10:56 of 2016.01.29 .
+ */
+public interface Strategy {
+    public int strategy();
+}
