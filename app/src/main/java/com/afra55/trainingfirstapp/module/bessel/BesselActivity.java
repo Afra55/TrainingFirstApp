59ea2ab55a178a665386e2706eb4fb4ef1c4fe98
Afra55 <1937525930@qq.com>
2016-10-19 14:20:24 +0800

    Bessel 1.

---
+++ app/src/main/java/com/afra55/trainingfirstapp/module/bessel/BesselActivity.java
@@ -0,0 +1,15 @@
+package com.afra55.trainingfirstapp.module.bessel;
+
+import android.os.Bundle;
+
+import com.afra55.trainingfirstapp.R;
+import com.afra55.trainingfirstapp.base.BaseActivity;
+
+public class BesselActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentLayout(R.layout.activity_bessel);
+    }
+}
